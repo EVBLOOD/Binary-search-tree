43f42952d7d81f934ce5e55c7d242cd511cd5338
Saad Akllam <sakllam@e2r7p13.1337.ma>
2022-08-17 12:41:57 +0100

    dealocating done.

--- BST.cpp
+++ BST.cpp
@@ -6,10 +6,11 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 18:09:49 by sakllam           #+#    #+#             */
-/*   Updated: 2022/08/16 20:22:30 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/08/17 12:38:47 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstdlib>
 #include <iostream>
 #include "bst_body.hpp"
 
@@ -23,9 +24,11 @@ int main()
     root.insert(10);
     root.insert(20);
     root.insert(17);
-    root.insert(17);
-    std::cout << root.find(0) << "\n";
-    std::cout << root.find(-1) << "\n";
-    std::cout << root.find(20) << "\n";
-    root.printing();
+    // root.insert(17);
+    // std::cout << root.find(0) << "\n";
+    // std::cout << root.find(-1) << "\n";
+    // std::cout << root.find(20) << "\n";
+    // root.printing();
+    root.free_tree();
+    // system("leaks a.out");
 }
--- bst_body.hpp
+++ bst_body.hpp
@@ -6,11 +6,12 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 19:26:51 by sakllam           #+#    #+#             */
-/*   Updated: 2022/08/16 21:11:57 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/08/17 12:41:13 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <algorithm>
 #include <memory>
 #include <cstddef>
 #include <string>
@@ -20,20 +21,23 @@
 
 namespace ft
 {
-    template <class x = int>
+    template <class x>
         struct bst
     {
             typedef x type;
             typedef bst<type> my_bst;
 
             type value;
-            my_bst *left;
             my_bst *right;
+            my_bst *left;
             bst(type value) : value(value), right(NULL), left(NULL) {}
+            bst() : value(type()) , right(NULL), left(NULL) {}
     };
+    
     template <class T, class Compare = std::less<T>, class Alloc = std::allocator<bst<T> > >
         class bst_body
     {
+        public:
             typedef Alloc allocator_type;
             typedef T value_type;
             typedef Compare cmp;
@@ -55,10 +59,16 @@ namespace ft
                 else
                     inserthelper(&((*root)->right), _new);
             }
-            void free_tree(bst<T> *x)
+
+            void helperfree_tree(bst<T> *x)
             {
-                
+                if (x == NULL)
+                    return;
+                helperfree_tree(x->right);
+                helperfree_tree(x->left);
+                alloc.deallocate(x, 1);
             }
+            
             void printinghelper(bst<T> *root, std::string name, int i)
             {
                 if (!root)
@@ -67,6 +77,7 @@ namespace ft
                 std::cout << i << "  " <<  name << " :" << root->value << std::endl;
                 printinghelper(root->right, "right", i + 1);
             }
+            
             bool findhelper(bst<T> *root, T value)
             {
                 if (root == NULL)
@@ -75,13 +86,16 @@ namespace ft
                     return true;
                 return findhelper(root->left, value) || findhelper(root->right, value);
             }
+            
             public:
                 bst_body() : head(NULL), size(0) {};
+                
                 ~bst_body()
                 {
-                    // alloc.deallocate(head, this->size);
-                    free_tree(this->head);
+                    if (head)
+                        free_tree();
                 }
+                
                 void insert(T el)
                 {
                     if (find(el))
@@ -89,26 +103,36 @@ namespace ft
                     inserthelper(&(this->head), makenew(el));
                     size++;
                 }
+                
                 bool find(T value)
                 {
                     return findhelper(this->head, value);
                 }
+                
                 bst<T>    *makenew(T value)
                 {
                     bst<T> *c = alloc.allocate(1);
                     c->value = value;
-                    c->left = c->right = NULL;
+                    c->left = NULL;
+                    c->right = NULL;
                     return c;
                 }
+                
                 void printing()
                 {
                     if (size)
                         printinghelper(this->head, "root", 0);
                 }
+                
                 size_t  Size()
                 {
                     return size;
                 }
-            
+                
+                void free_tree()
+                {
+                    helperfree_tree((this->head));
+                    head = NULL;
+                }
     };
 }
